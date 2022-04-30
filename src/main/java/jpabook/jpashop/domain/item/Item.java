d3d1981534d30f9705aa19c442e2743bc3f4a6e1
taxol1203 <taxol1203@pusan.ac.kr>
2022-04-30 21:19:08 +0900

    feat : Add Item Entity's bussiness rogic

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 비즈니스 로직을 엔티티에 두어, 응집도를 높인다. <- 데이터를 가지고 있는 쪽에 비즈니스 로직이 있는 것이 좋다
+    /**
+     * 재고 증가
+    * */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 감소
+     * */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
