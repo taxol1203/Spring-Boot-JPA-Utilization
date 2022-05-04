e84caf6fac45c486ff4864c064f3f8820b097ced
taxol1203 <taxol1203@pusan.ac.kr>
2022-05-04 22:10:29 +0900

    feat : Create Entity Method for OrderItem, order

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -46,4 +46,45 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems){
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems){
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    //==비즈니스 로직==//
+    /**
+     * 주문 취소
+    */
+    public void cancel(){
+        if(delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);     // 해당 주문은 이제 취소 상태이다.
+        for(OrderItem orderItem : orderItems){
+            orderItem.cancel();             // 취소하였으므로, 처음 주문하여서 빠졌던 item 수 만큼 다시 채워준다.
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -23,6 +23,32 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private Long orderPrice;        // 주문 가격
-    private Long count;             // 주문 수량량
+    private int orderPrice;        // 주문 가격, 이미 item이 있는데 왜? => 할인 받을 수도 있으니
+    private int count;             // 주문 수량량
+
+    //==생성 메서드==//
+    public static OrderItem createdOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);        // 주문한 양 만큼 재고를 까줘야한다.
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);      // 취소하였으므로, 처음 주문하여서 빠졌던 item 수 만큼 다시 채워준다.
+    }
+
+    //== 조회 로직 == //
+
+    /**
+     * 주문 상품 전체 가격 조회
+     * @return 주문상품 전체 가격
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
